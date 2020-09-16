651803282305af60c7ad125c328c274fb0ee7391
Dai Pham <dai@Dais-MacBook-Pro-2.local>
2020-09-16 15:29:39 +0700

    hello

--- receiver.js
+++ receiver.js
@@ -4,7 +4,7 @@ const playerManager = context.getPlayerManager();
 
 // Update style using javascript
 let playerElement = document.getElementsByTagName("cast-media-player")[0];
-playerElement.style.setProperty('--splash-image', 'url("image.png")');
+playerElement.style.setProperty('--splash-image', 'url("https://raw.githubusercontent.com/phamhuudai/ChromecastReceiver/master/icon.png")');
 
 
 context.start();
