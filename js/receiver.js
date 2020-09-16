74b3ef61bee158815cf26fdc416bfee3a7140a38
Dai Pham <dai@Dais-MacBook-Pro-2.local>
2020-09-16 11:57:21 +0700

    xxs

--- js/receiver.js
+++ js/receiver.js
@@ -3,7 +3,7 @@ const context = cast.framework.CastReceiverContext.getInstance();
 
 // Update style using javascript
 let playerElement = document.getElementsByTagName("cast-media-player")[0];
-playerElement.style.setProperty('--splash-image', 'url("/icon.png")');
+playerElement.style.setProperty('--splash-image', 'url("./icon.png")');
 
 
 context.start();
