204831adb4bb4be8007ce9d10ddf450c09fad89d
Dai Pham <dai@Dais-MacBook-Pro-2.local>
2020-09-16 11:34:04 +0700

    dsdsd

---
+++ js/receiver.js
@@ -0,0 +1,9 @@
+const context = cast.framework.CastReceiverContext.getInstance();
+
+
+// Update style using javascript
+let playerElement = document.getElementsByTagName("cast-media-player")[0];
+playerElement.style.setProperty('--splash-image', 'url("http://some/other/image.png")');
+
+
+context.start();
